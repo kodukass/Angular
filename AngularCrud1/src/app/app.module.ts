62f19821e9d85c47499d997cccc2b43a0cd1eda1
Ingvar Derešivski <inde999@gmail.com>
2022-06-10 14:27:41 +0300

    Init

---
+++ AngularCrud1/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListEmployeesComponent } from './employees/list-employees.component';
+import { CreateEmployeeComponent } from './employees/create-employee.component';
+import { RouterModule, Routes } from '@angular/router';
+//import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+
+
+const appRoutes: Routes = [
+  {path: 'list', component: ListEmployeesComponent},
+  {path: 'create', component: CreateEmployeeComponent},
+  {path: '', redirectTo:'/list', pathMatch: 'full'}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListEmployeesComponent,
+    CreateEmployeeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    //BsDatepickerModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ AngularCrud1/src/app/employees/create-employee.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Department } from '../models/department.model';
+import { Employee } from '../models/employee.model';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.scss']
+})
+export class CreateEmployeeComponent implements OnInit {
+
+  previewPhoto = false;
+
+  employee: Employee = {
+    id: 0,
+    fullname: '',
+    gender: '', 
+    email: '',
+    phoneNumber: 0,
+    contactPreference: '',
+    dateOfBirth: new Date(),
+    department: '',
+    isActive: false,
+    photoPath: '',
+  };
+
+  departments: Department[] = [
+    { id: 1, name: 'Help Desk'},
+    { id: 2, name: 'HR'},
+    { id: 3, name: 'IT'},
+    { id: 4, name: 'Payroll'},
+    { id: 5, name: 'Admin'}
+  ];
+  
+  constructor() {
+  }
+
+  togglePhotoPreview() {
+    this.previewPhoto = !this.previewPhoto;
+  }
+
+  ngOnInit(): void {
+  }
+
+  saveEmployee(empForm: NgForm): void {
+    console.log(empForm.value);
+  }
+
+}
